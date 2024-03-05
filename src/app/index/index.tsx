3ea3e3716cba8d4c36033e6153a88ee53c75b99a
Hideaki Gabriel <hideaki.gabriel@gmail.com>
2024-03-05 01:49:21 -0300

    chore: first setup app

---
+++ src/app/index/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import { styles } from "./styles"
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Escolha os produtos</Text>
+      <Text style={styles.subtitle}>Descubra receitas baseada nos produtos que você escolheu.</Text>
+    </View>
+  )
+}
