0e6a70619be6e9e944d818df44f78fa3daff6c98
Hideaki Gabriel <hideaki.gabriel@gmail.com>
2024-03-06 22:03:52 -0300

    feat: add Poppins font  on theme

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -4,8 +4,13 @@ import { styles } from "./styles"
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Escolha os produtos</Text>
-      <Text style={styles.subtitle}>Descubra receitas baseada nos produtos que você escolheu.</Text>
+      <Text style={styles.title}>
+        Escolha {"\n"} 
+        <Text style={styles.subtitle}>os produtos</Text>
+      </Text>
+      <Text style={styles.message}>
+        Descubra receitas baseada nos produtos que você escolheu.
+      </Text>
     </View>
   )
 }
