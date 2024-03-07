f3f822e5fc459b7bc857478126eae0763f835d04
Hideaki Gabriel <hideaki.gabriel@gmail.com>
2024-03-07 00:54:46 -0300

    feat: add handleToggleSelected function

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,8 +1,19 @@
 import { View, Text, ScrollView } from "react-native";
 import { styles } from "./styles";
 import Ingredient from "@/components/ingredient";
+import { useState } from "react";
 
 export default function Index() {
+  const [ selected, setSelected ] = useState<string[]>([])
+
+  function handleToggleSelected(value: string) {
+    if(selected.includes(value)) {
+      return setSelected((state) => state.filter((item) => item !== value))
+    }
+
+    setSelected((state) => [...state, value])
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
@@ -16,9 +27,16 @@ export default function Index() {
       contentContainerStyle={styles.ingredients}
       showsVerticalScrollIndicator={false}
       >
-        {Array.from({length: 100}).map((item, index) => (
-          <Ingredient key={index} />
-))}
+        {
+           Array.from({length: 100}).map((item, index) => (
+           <Ingredient 
+              name="Maça" 
+              image="" 
+              selected={selected.includes(String(index))}
+              key={index} 
+              onPress={() => handleToggleSelected(String(index))}
+           />
+        ))}
       </ScrollView>
     </View>
   );
--- src/components/ingredient/index.tsx
+++ src/components/ingredient/index.tsx
@@ -1,9 +1,18 @@
-import { Image, Pressable, Text } from "react-native";
+import { Image, Pressable, PressableProps, Text } from "react-native";
 import { styles } from "./style";
 
-export default function Ingredient(){
+export type IngredientsProps = {
+  name: String;
+  image: String;
+  selected?: boolean
+}
+
+export default function Ingredient({name, image, selected = false, ...rest}: IngredientsProps & PressableProps){
   return (
-    <Pressable style={styles.container}>
+    <Pressable 
+    style={[styles.container, selected && styles.selected]}
+    {...rest}
+    >
       <Image source={require("@/assets/apple.png")} style={styles.image}/>
       <Text style={styles.title}>Maça</Text>
     </Pressable>
